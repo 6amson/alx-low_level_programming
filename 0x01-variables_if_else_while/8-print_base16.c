0dad8e923788f2741ea8f9eb06a419fd3db142ab
bunmigrey <bunmigrey@icloud.com>
2022-03-12 11:08:26 +0000

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabeth in lower case
+ *
+ * Decription: using the scrip prints alphabeth in lower case
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0')
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
