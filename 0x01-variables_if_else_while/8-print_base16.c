6401b67b25fa3d6e5ebd1c57908ccd1594644e55
bunmigrey <bunmigrey@icloud.com>
2022-03-12 11:11:25 +0000

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 	char letter;
 
 	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0')
+		putchar((num % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
