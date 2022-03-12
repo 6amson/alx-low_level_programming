235fe6705a963fe38daf1cfd6a83fa2c02223e31
bunmigrey <bunmigrey@icloud.com>
2022-03-12 09:48:44 +0000

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints alphabeth in lower case
+ *
+ * Decription: using the scrip prints alphabeth in lower case
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter)
+
+			putchar('\n');
+	return (0);
+}
