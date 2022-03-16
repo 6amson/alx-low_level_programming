e2a8964edabe2e8bd55967c8e4773b42284f2dac
bunmigrey <bunmigrey@icloud.com>
2022-03-16 20:02:02 +0000

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * print_alpahabet - function that prints the alphabet, in lowercase,
+ * followed by a new line.
+ *
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+_putchar('\n');
+
+
+}
