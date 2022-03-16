c505e1c80500e42fd3730fa97bc37875c0d22161
bunmigrey <bunmigrey@icloud.com>
2022-03-16 12:18:49 +0000

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * main - prints _putchar, followed by a new line.
+ * Description - a script that does as stated above.
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char Z[] = "_putchar\n";
+	int t = 0;
+
+	while (t < 9)
+	{
+		_putchar(Z[t]);
+		t++;
+	}
+
+	return (0);
+}
