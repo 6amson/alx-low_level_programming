4e6a2911b616109c22fc379148d0ce2b5cdfb2cd
bunmigrey <99497635+bunmigrey@users.noreply.github.com>
2022-03-21 20:14:37 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - A program that takes a pointer to an integer
+ * @n: The input variable
+ * Return : void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
